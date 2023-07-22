1db5afe066c76b1d561b45c7b1c7df7e415ec049
Matvitsevsky <ilya.matvitsevsky@vsgcommerce.com>
2023-07-22 16:09:58 +0300

    add cors to main file

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,6 +16,11 @@ async function bootstrap() {
   app.use(passport.initialize());
   app.use(passport.session());
 
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+
   const config = new DocumentBuilder()
     .setTitle('ToDo')
     .setDescription('api documentation')
